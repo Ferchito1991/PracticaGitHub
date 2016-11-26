3dee682f7eaa2adff5cb2f5d55542e29f52e8507
Fernando Martínez <FerMC_1991@hotmail.com>
2016-11-26 12:02:49 -0600

    Epic - Agregado Sumar

---
+++ Sumar/src/sumar/Sumar.java
@@ -0,0 +1,17 @@
+package sumar;
+
+public class Sumar {
+
+    public static void main(String[] args) {
+        int NumeroA = 0;
+        int NumeroB = 0;       
+        sumar(NumeroA,NumeroB);
+    }
+    
+    private static int sumar (int a, int b){
+        int resultado = 0;
+        resultado = a+b;
+        return resultado;
+    }
+    
+}
